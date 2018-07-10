57208e075404f79ea5924bfa20350753fe96ecc9
Anish-Jajodia <hello@lomdi.in>
2018-07-10 15:26:56 +0530

    Revert "setting page integration in one"

--- checklogin.php
+++ checklogin.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 session_start();
 require __DIR__.'/connection.php'; //DB connectivity
 	$shop_url = $_POST['shop_url'];
@@ -11,7 +11,7 @@ require __DIR__.'/connection.php'; //DB connectivity
 			$user_exist = pg_query($dbconn4, "UPDATE user_table SET access_key ='{$access_key}' , email='{$email}' , password='{$password}'  WHERE store_url = '{$shop_url}'");
 				if($user_exist){
 					echo "login details update  sucessfully";
-
+					
 				}
 		}
 		else {
@@ -22,22 +22,19 @@ require __DIR__.'/connection.php'; //DB connectivity
 			}
 		}
 	}
-
+	
 		if(isset($_REQUEST['getaccesstoken']) || $_REQUEST['getaccesstoken']!=''){
 			$user_exist = pg_query($dbconn4, "SELECT * FROM user_table WHERE store_url = '{$shop_url}'");
 			if(pg_num_rows($user_exist)){
 
 				while ($row = pg_fetch_assoc($user_exist)) {
-						 // echo $row['access_key'];
-						 echo "<p id='access_key'>".$row['access_key']."</p>";
-						 echo "<p id='ship_logo'>".$row['logo']."</p>";
-						 echo "<p id='ship_preference_company'>".$row['ship_preference']."</p>";
+						 echo $row['access_key'];
 					}
 			}
 	  	}
+	
 
-
-
+        	
 		if(isset($_REQUEST['saveaddress']) || $_REQUEST['saveaddress']!=''){
 
 			$address_id=trim($_REQUEST['saveaddress']);
@@ -56,12 +53,12 @@ require __DIR__.'/connection.php'; //DB connectivity
 				//echo "UPDATE pickup_address SET name ='{$name}' , address_line1='{$address_line1}', address_line2='{$address_line2}', city='{$city}', zipcode='{$zipcode}', phoneno='{$phoneno}',companyname='{$companyname}' WHERE shop_url='{$shop_url}' and id={$address_id}";
 		           $sql =   pg_query($dbconn4, "UPDATE pickup_address SET name ='{$name}' , address_line1='{$address_line1}', address_line2='{$address_line2}', city='{$city}', zipcode='{$zipcode}', phoneno='{$phoneno}',companyname='{$companyname}' WHERE shop_url='{$shop_url}' and id={$address_id}");
 				echo "Address update  sucessfully";
-
-
+				
+				
 			}
 			else{
 			 $sql = "insert into pickup_address (name,address_line1,address_line2,city ,zipcode,phoneno,shop_url,companyname) values ('$name','$address_line1','$address_line2','$city', '$zipcode','$phoneno','$shop_url','$companyname' )";
-				$qry = pg_query($sql);
+				$qry = pg_query($sql);	
 				if($qry){
 				echo "Address save sucessfully";
 				}
